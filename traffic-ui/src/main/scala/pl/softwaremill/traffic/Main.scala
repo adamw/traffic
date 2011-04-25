831cc57934249ca450ac72283c7e1d7a2a8bf106
adamw <adam@warski.org>
2011-04-25 13:05:44 +0200

    Refactoring

--- traffic-ui/src/main/scala/pl/softwaremill/traffic/Main.scala
+++ traffic-ui/src/main/scala/pl/softwaremill/traffic/Main.scala
@@ -12,50 +12,60 @@ object Main {
 }
 
 class Main extends ProxiedApplet {
-  trait ViewDefinitionComponentConfigured extends ViewDefinitionComponent {
-    val viewDefinition = new ViewDefinition {
-      val heightSpan = 100.meters
-      val widthSpan = 200.meters
-      val heightPixels = 200
-      val widthPixels = 600
+  type UIComponents = DrawerComponent with MouseEventsComponent with ViewDefinitionComponent
+
+  def createEnv: UIComponents = {
+    trait ViewDefinitionComponentConfigured extends ViewDefinitionComponent {
+      val viewDefinition = new ViewDefinition {
+        val heightSpan = 100.meters
+        val widthSpan = 200.meters
+        val heightPixels = 200
+        val widthPixels = 600
+      }
+    }
+
+    trait GfxComponentConfigured extends GfxComponent {
+      val gfx = Main.this
+    }
+
+    val env = new DrawerComponent
+            with MouseEventsComponent
+            with GfxComponentConfigured
+            with ViewDefinitionComponentConfigured
+            with SpanToPixelsTranslatorComponent
+            with UIModelComponent
+            with SimulationStateComponent
+
+    env.updateState(env.SimulationState(
+      Vehicle(TypicalCar, Position(10.meters, 10.meters), 0.degrees, Speed(60.kilometers, 1.hour)) ::
+              Vehicle(TypicalCar, Position(10.meters, 20.meters), 0.degrees, Speed(30.kilometers, 1.hour)) :: Nil,
+      Barrier(Position(40.meters, 5.meters), Position(41.meters, 25.meters), Barrier.Green) :: Nil))
+
+    env
+  }
+
+  def createDrawProxy(env: UIComponents) = {
+    new DrawProxy(this) {
+      size(env.viewDefinition.widthPixels, env.viewDefinition.heightPixels)
+      frameRate = 20f
+
+      var lastDraw = 0L
+
+      def draw() {
+        val now = System.currentTimeMillis
+        if (lastDraw == 0L) lastDraw = now
+        env.drawer.step(Period.millis((now-lastDraw).toInt))
+
+        lastDraw = now
+      }
+
+      override def mouseClicked() {
+        env.mouseEvents.mouseClicked(mouseX, mouseY)
+      }
     }
   }
 
-  trait GfxComponentConfigured extends GfxComponent {
-    val gfx = Main.this
-  }
-
-  val env = new DrawerComponent
-          with MouseEventsComponent
-          with GfxComponentConfigured
-          with ViewDefinitionComponentConfigured
-          with SpanToPixelsTranslatorComponent
-          with UIModelComponent
-          with SimulationStateComponent
-
-  env.updateState(env.SimulationState(
-    Vehicle(TypicalCar, Position(10.meters, 10.meters), 0.degrees, Speed(60.kilometers, 1.hour)) ::
-    Vehicle(TypicalCar, Position(10.meters, 20.meters), 0.degrees, Speed(30.kilometers, 1.hour)) :: Nil,
-    Barrier(Position(40.meters, 5.meters), Position(41.meters, 25.meters), Barrier.Green) :: Nil))
-
-  lazy val px = new DrawProxy(this) {
-    size(env.viewDefinition.widthPixels, env.viewDefinition.heightPixels)
-    frameRate = 20f
-
-    var lastDraw = 0L
-
-    def draw() {
-      val now = System.currentTimeMillis
-      if (lastDraw == 0L) lastDraw = now
-      env.drawer.step(Period.millis((now-lastDraw).toInt))
-
-      lastDraw = now
-    }
-
-    override def mouseClicked() {
-      env.mouseEvents.mouseClicked(mouseX, mouseY)
-    }
-  }
+  lazy val px = createDrawProxy(createEnv)
 }
 
 trait GfxComponent {
